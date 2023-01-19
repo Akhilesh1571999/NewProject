be85cf648b8b9d3148d927d87c32ebe363d57ad8
akhilesh <akhileshgargatte@gmail.com>
2023-01-19 11:59:03 +0530

    Demo1 changed

--- src/com/velocity/Demo1.java
+++ src/com/velocity/Demo1.java
@@ -4,9 +4,7 @@ public class Demo1 {
 
 	public static void main(String[] args) {
 		int []a = {1,2,3,6,5,4,9,8,7};
-		for(int x:a) {
-			System.out.println(x);
-		}
+		
 		for(int i =0; i<a.length; i++) {
 			System.out.println(a[i]);
 		}
